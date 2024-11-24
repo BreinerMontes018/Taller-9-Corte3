80f574eedce57fbafd7c9a35034c804805fc00e5
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-11-23 21:09:20 -0500

    Taller 9

---
+++ Taller9Poo/Ejercicio3/Empleado.java
@@ -0,0 +1,18 @@
+package Taller9Poo.Ejercicio3;
+
+class Persona {
+    /*private String nombre; 
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+        */
+}
+
+public class Empleado extends Persona {
+    public Empleado(String nombre) {
+        // super(nombre);
+        // no se puede acceder a nombre aqui
+        //System.out.println(super.nombre);
+    }
+}
